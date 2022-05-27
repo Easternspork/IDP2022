44e0616efc52838f61fcc3c42c1c073897bf1618
easternspork <fumi.okutsu@gmail.com>
2022-05-27 16:10:30 -0700

    mugshot and name stuff

--- Assets/Scripts/Mugshot.cs
+++ Assets/Scripts/Mugshot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Mugshot : MonoBehaviour
 {
@@ -15,19 +16,71 @@ public class Mugshot : MonoBehaviour
     [SerializeField]
     private List<PlayableDirector> timelines;
 
+
+    [SerializeField]
+    private List<PlayableDirector> timelinesOut;
+
     private int characterNumber = 0;
 
     [SerializeField]
     private TextMeshProUGUI characterName;
 
-    public void ShowNext()
+    [SerializeField]
+    private TextMeshProUGUI characterDescription;
+
+    [SerializeField]
+    private GameObject inputField;
+
+    [SerializeField]
+    private TMP_InputField inputName;
+    
+    [SerializeField]
+    private GameObject nextButton;
+
+    [SerializeField]
+    private GameObject mugshotCanvas;
+
+    [SerializeField]
+    private GameObject descriptionThing;
+    
+    [SerializeField]
+    private GameObject nameThing;
+
+    private void Start()
     {
         
-        characterNumber++;
+    }
+
+
+    public void ShowNext()
+    {
+        Debug.Log("bruh");
+        mugshotCanvas.SetActive(true);
+        characterName.text = names[characterNumber];
+        characterDescription.text = names[characterNumber];
+
     }
     
     public void AskMain()
     {
+        mugshotCanvas.SetActive(true);
+        inputField.SetActive(true);
+    }
 
+    public void OnNextPressed()
+    {
+
+        mugshotCanvas.SetActive(false);
+        timelinesOut[characterNumber].Play();
+        characterNumber++;
+        timelines[characterNumber].Play();
+        
+
+    }
+
+    public void OnEnterPressed()
+    {
+        GameManager.GetInstance().playerName = inputName.text;
+        SceneManager.LoadScene("Cell");
     }
 }
