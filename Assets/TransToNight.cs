41285d8eadf47d55690bd8fd6c62ef9e1a875dab
easternspork <lazoorcraft@gmail.com>
2022-06-01 22:45:49 -0700

    add trans to night time

---
+++ Assets/TransToNight.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransToNight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToNight()
+    {
+        SceneManager.LoadScene("Nighttime");
+    }
+    public void GoToPrison()
+    {
+        SceneManager.LoadScene("Cell");
+    }
+}
